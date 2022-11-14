61c38c5e6d234dd43b144c47775fcc1231c4fd3f
Quang Hà <btkkentxx2002@gmail.com>
2022-11-14 08:50:56 +0700

    boosttrap

---
+++ auth/fail.js
@@ -0,0 +1,8 @@
+// Alert Modal Type
+$(document).on('click', '#success', function(e) {
+    swal(
+        'Success',
+        'You clicked the <b style="color:green;">Success</b> button!',
+        'success'
+    )
+});
